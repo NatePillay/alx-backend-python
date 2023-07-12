06348b24c790789215849dab5e2af7947a5a5eda
NatePillay <nathan94pillay@gmail.com>
2023-07-12 03:44:02 +0000

    new commit

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Task 5's module.
+'''
+
+from typing import List
+
+def sum_list(input_list: List[float]) -> float:
+    '''func to sum list of floats'''
+    return float(sum(input_list))
